bd0111e1a4ae14df12d60e8c5554244ea6ea6f68
Michel Motta da Silva <michelmottadasilva@gmail.com>
2019-02-26 23:06:26 -0400

    Generating migrations files and Models

---
+++ db/migrate/20190227025928_create_teachers.rb
@@ -0,0 +1,13 @@
+class CreateTeachers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :teachers do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.string :subject
+      t.references :school, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
