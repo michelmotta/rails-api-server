c511fc4ebe9c6325d102234527e89487c94e76f6
Michel Motta da Silva <michelmottadasilva@gmail.com>
2019-02-27 01:14:18 -0400

    Added CRUD methods in Schools Controller

--- app/controllers/api/v1/schools_controller.rb
+++ app/controllers/api/v1/schools_controller.rb
@@ -1,9 +1,66 @@
 module Api
     module V1
         class SchoolsController < ApplicationController
+            
+            ##
+            # Index Method
+            # Method used by API to return a list of schools from database
+            #
             def index
-                schools = School.order('created_at DESC');
-                render json: {status: 'SUCCESS', message: 'Schools List', data: schools}, status: :ok
+                schools = School.order('created_at DESC')
+                render json: {status: 'SUCCESS', message: 'Loaded school list', data: schools}, status: :ok
+            end
+
+            ##
+            # Show Method
+            # Method used by API to return a school from database
+            #
+            def show
+                school = School.find(params[:id])
+                render json: {status: 'SUCCESS', message: 'Loaded School', data: school}, status: :ok
+            end
+
+            ##
+            # Create Method
+            # Method used by API to create new school into database
+            #
+            def create
+                school = School.new(school_params)
+                if school.save
+                    render json: {status: 'SUCCESS', message: 'School has been saved.', data: school}, status: :ok
+                else
+                    render json: {status: 'ERROR', message: 'School has not been saved.', data: school.errors}, status: :unprocessable_entity
+                end
+            end
+
+            ##
+            # Update Method
+            # Method used by API to update a school into database
+            #
+            def update
+                school = School.find(params[:id])
+                if school.update_attributes(school_params)
+                    render json: {status: 'SUCCESS', message: 'School has been updated.', data: school}, status: :ok
+                else
+                    render json: {status: 'ERROR', message: 'School has not been updated.', data: school.errors}, status: :unprocessable_entity
+            
+                end
+            end
+
+            ##
+            # Destroy Method
+            # Method used by API to delete a school from database
+            #
+            def destroy
+                school = School.find(params[:id])
+                school.destroy
+                render json: {status: 'SUCCESS', message: 'School has been deleted.', data: school}, status: :ok
+            end
+
+            private
+
+            def school_params
+                params.permit(:name, :email, :phone)
             end
         end
     end
