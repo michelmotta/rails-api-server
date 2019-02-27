7a49ae1d65607f97ac5c32cdbd7d1ceff165f09a
Michel Motta da Silva <michelmottadasilva@gmail.com>
2019-02-27 02:24:08 -0400

    Added new Teachers Controller and modified routes

---
+++ app/controllers/api/v1/teachers_controller.rb
@@ -0,0 +1,71 @@
+module Api
+    module V1
+        class TeachersController < ApplicationController
+
+            ##
+            # Index Method
+            # Method used by API to return a list of teachers from database
+            # GET /api/v1/teachers
+            def index
+                teachers = Teacher.order('created_at DESC')
+                render json: {status: 'SUCCESS', message: 'Loaded teacher list', data: teachers}, status: :ok
+            end
+
+            ##
+            # Show Method
+            # Method used by API to return a teacher from database
+            # GET /api/v1/teachers/[id]
+            def show
+                teacher = Teacher.find(params[:id])
+                render json: {status: 'SUCCESS', message: 'Loaded teacher', data: teacher}, status: :ok
+            end
+
+            ##
+            # Create Method
+            # Method used by API to create new teacher into database
+            # POST /api/v1/teachers
+            def create
+                teacher = Teacher.new(teacher_params)
+                if teacher.save
+                    render json: {status: 'SUCCESS', message: 'Teacher has been saved.', data: teacher}, status: :ok
+                else
+                    render json: {status: 'ERROR', message: 'Teacher has not been saved.', data: teacher.errors}, status: :unprocessable_entity
+                end
+            end
+
+            ##
+            # Update Method
+            # Method used by API to update a teacher into database
+            # PUT /api/v1/teachers/[id]
+            def update
+                teacher = Teacher.find(params[:id])
+                if teacher.update_attributes(teacher_params)
+                    render json: {status: 'SUCCESS', message: 'Teacher has been updated.', data: teacher}, status: :ok
+                else
+                    render json: {status: 'ERROR', message: 'Teacher has not been updated.', data: teacher.errors}, status: :unprocessable_entity
+            
+                end
+            end
+
+            ##
+            # Destroy Method
+            # Method used by API to delete a teacher from database
+            # DELETE /api/v1/teachers/[id]
+            def destroy
+                teacher = Teacher.find(params[:id])
+                teacher.destroy
+                render json: {status: 'SUCCESS', message: 'Teacher has been deleted.', data: teacher}, status: :ok
+            end
+
+            private
+
+            ##
+            # Method teacher_params 
+            # Private params for Teacher
+            def teacher_params
+                params.permit(:name, :email, :phone, :subject)
+            end
+
+        end
+    end
+end
